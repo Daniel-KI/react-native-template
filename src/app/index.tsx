b2a1b31c3cd9e0dff8d041a1bb87753be73ca280
Daniil Karshou <dankorshov@gmail.com>
2025-04-13 22:55:36 +0300

    feat: add react-native-reusables

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@/components/ui/button';
 import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View className='flex justify-center items-center'>
       <Text className='t text-red-800'>Home</Text>
+      <Button variant='destructive' size='lg'>
+        <Text>Default</Text>
+      </Button>
     </View>
   );
 }
